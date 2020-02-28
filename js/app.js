f344e9b6332717dea96eafe8718bc2daa3a15b50
Prashant Kumar Singh <pks37216@usaunivquest.com>
2020-02-28 12:59:08 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('working');
+function Algorithm(type, strategy) {
+    this.type = type;
+    this.strategy = strategy;
+    this.dataSetSize = document.getElementById("dataset_size").value || 10;
+    this.title = this.strategy + " " + this.type;
+
+    this.changeSpeed = () => {
+        return parseInt(document.getElementById('speed').dataset.seconds);
+    }
+
+    this.timeDelay = async (speed) => {
+        return new Promise(resolve =>
+            setTimeout(() => {
+              resolve();
+            }, speed)
+          );
+    }
+}
